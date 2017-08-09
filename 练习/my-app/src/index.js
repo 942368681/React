d8bf403fc2cce17abc4a8490eec1e4ccb7d72ad0
Sfl <942368681@qq.com>
2017-08-09 23:51:52 +0800

    demo

---
+++ 练习/my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+import Ul from './demo1.js';
+import Ul2 from './demo2.js'
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Ul2 />, document.getElementById('root'));
+registerServiceWorker();
